bb004d296c3f3217e49cad161fa7a85b63a454af
Jona <jonathanelian64@gmail.com>
2023-01-05 21:48:31 -0700

    [TEST] add test for route /healthcheck

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use axum::{
     routing::{get, post},
     Router,
 };
+use store::Store;
 
 mod resources;
 mod store;
@@ -12,6 +13,22 @@ mod types;
 async fn healthcheck() -> &'static str {
     "OK"
 }
+
+fn get_router(store: Store) -> Router {
+    Router::new()
+        .route("/healthcheck", get(healthcheck))
+        .route("/auth/login", post(resources::auth::login_user))
+        .route("/auth/register", post(resources::auth::register_user))
+        .with_state(store)
+}
+
+#[allow(dead_code)]
+fn test_router() -> Router {
+  Router::new()
+      .route("/healthcheck", get(healthcheck))
+      .route("/auth/login", post(resources::auth::login_user))
+}
+
 
 #[tokio::main]
 async fn main() {
@@ -22,11 +39,7 @@ async fn main() {
         .await
         .expect("Cannot run migration");
 
-    let app = Router::new()
-        .route("/healthcheck", get(healthcheck))
-        .route("/auth/login", post(resources::auth::login_user))
-        .route("/auth/register", post(resources::auth::register_user))
-        .with_state(store);
+    let app = get_router(store);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
@@ -38,8 +51,21 @@ async fn main() {
 
 #[cfg(test)]
 mod tests {
+    use axum::http::StatusCode;
+    use axum_test_helper::TestClient;
+
     #[tokio::test]
-    async fn test_checkhealth() {
+    async fn test_healthcheck_route() {
+        let app = super::test_router();
+        let client = TestClient::new(app);
+        let res = client.get("/healthcheck").send().await;
+        assert_eq!(res.status(), StatusCode::OK);
+        assert_eq!(res.text().await, "OK");
+
+    }
+
+    #[tokio::test]
+    async fn test_healthcheck() {
         let health = super::healthcheck().await;
         assert_eq!(health, "OK");
     }
