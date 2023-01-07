50584583b188e6b8cc087160082ef339231eeaf3
h8a <herii8a@gmail.com>
2023-01-06 23:26:50 -0700

    [ADD] read settings variables for connection postgres

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::net::SocketAddr;
-
 use axum::{
+    middleware,
     routing::{get, post},
     Router,
 };
+use dotenv;
 
+mod middleware_hooks;
 mod resources;
 mod types;
 mod store;
@@ -16,7 +18,12 @@ async fn healthcheck() -> &'static str {
 #[tokio::main]
 async fn main() {
 
-    let store = store::Store::new("postgres://test_user:test_password@localhost:5411/test1").await;
+    let store = store::Store::new(&format!("postgres://{}:{}@{}:{}/{}",
+        dotenv::var("DB_USER").unwrap(),
+        dotenv::var("DB_PASSWORD").unwrap(),
+        dotenv::var("DB_HOST").unwrap(),
+        dotenv::var("DB_PORT").unwrap(),
+        dotenv::var("DB_NAME").unwrap())).await;
 
     sqlx::migrate!()
         .run(&store.clone().connection)
@@ -27,7 +34,8 @@ async fn main() {
         .route("/healthcheck", get(healthcheck))
         .route("/auth/login", post(resources::auth::login_user))
         .route("/auth/register", post(resources::auth::register_user))
-        .with_state(store);
+        .with_state(store)
+        .layer(middleware::from_fn(middleware_hooks::auth::authorization));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
---
+++ src/middleware_hooks/auth.rs
@@ -0,0 +1,21 @@
+use axum::{http::{Request, StatusCode, header::AUTHORIZATION}, middleware::Next, response::Response};
+
+pub async fn authorization<B>(req: Request<B>, next: Next<B>) -> Result<Response, StatusCode> {
+    let auth_header = req.headers()
+        .get(AUTHORIZATION)
+        .and_then(|header| header.to_str().ok());
+
+    println!("{:?}", auth_header);
+
+    match auth_header {
+        Some(auth_header) if token_is_valid(auth_header) => {
+            Ok(next.run(req).await)
+        }
+        _ => Err(StatusCode::UNAUTHORIZED),
+    }
+}
+
+fn token_is_valid(token: &str) -> bool {
+    println!("{}", token);
+    true
+}
