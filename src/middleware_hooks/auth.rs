8929711d34a4b3775bb8625796f01353db2dce83
h8a <herii8a@gmail.com>
2023-01-10 23:12:25 -0700

    [ADD] valid token

--- src/middleware_hooks/auth.rs
+++ src/middleware_hooks/auth.rs
@@ -1,23 +1,54 @@
-use axum::{http::{Request, StatusCode, header::AUTHORIZATION}, middleware::Next, response::Response};
+use axum::{
+    http::{
+        Request,
+        StatusCode,
+        header::AUTHORIZATION
+    }, 
+    middleware::Next, 
+    response::Response
+};
+
+use crate::utils::security::jwt_decode;
 
 pub async fn authorization<B>(req: Request<B>, next: Next<B>) -> Result<Response, StatusCode> {
     let auth_header = req.headers()
         .get(AUTHORIZATION)
         .and_then(|header| header.to_str().ok());
 
-    println!("AUTH_HEADER: {:?}", auth_header);
+    match req.uri().path() {
+        "/auth/register" | "/auth/login" => {
+            Ok(next.run(req).await)
+        },
+        _ => {
+            let header_is_valid =  header_is_valid(auth_header);
+            if header_is_valid {
+                Ok(next.run(req).await)
+            } else {
+                Err(StatusCode::UNAUTHORIZED)
+            }
+        },
+    }
+}
 
-    Ok(next.run(req).await)
-
-    // match auth_header {
-    //     Some(auth_header) if token_is_valid(auth_header) => {
-    //         Ok(next.run(req).await)
-    //     }
-    //     _ => Err(StatusCode::UNAUTHORIZED),
-    // }
+fn header_is_valid(header: Option<&str>) -> bool {
+    match header {
+        Some(token) => {
+            let token: Vec<&str> = token.split(" ").collect();
+            let suffix = token[0];
+            if suffix == "Bearer" {
+                token_is_valid(token[1])
+            } else {
+                false
+            }
+        },
+        None => false
+    }
 }
 
 fn token_is_valid(token: &str) -> bool {
-    println!("{}", token);
-    true
+    let is_valid =  jwt_decode(token);
+    match is_valid {
+        Ok(_) => true,
+        Err(_) => false
+    }
 }
