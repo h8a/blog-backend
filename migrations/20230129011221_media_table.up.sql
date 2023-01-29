87d2eb35c3e07fc2026a0a4577e3a71de87a7928
h8a <herii8a@gmail.com>
2023-01-28 18:17:35 -0700

    [ADD] tables for media files

---
+++ migrations/20230129011221_media_table.up.sql
@@ -0,0 +1,9 @@
+-- Add up migration script here
+CREATE TABLE IF NOT EXISTS media (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (255) NOT NULL,
+    name_generated uuid NOT NULL,
+    path VARCHAR (255) NOT NULL,
+    content_type VARCHAR (255) NOT NULL,
+    created_on TIMESTAMP NOT NULL DEFAULT NOW()
+);
